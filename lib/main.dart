b78abb85401a9610af5a9afd2d33ca1d96272b19
Sodgerel Mandakhnaran <sm8694@nyu.edu>
2021-02-13 15:08:59 +0800

    Added new questions

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'question.dart';
 
 void main() => runApp(Quizzler());
 
@@ -25,15 +26,37 @@ class QuizPage extends StatefulWidget {
 }
 
 class _QuizPageState extends State<QuizPage> {
-  var questions = [
-    'Paris is the capital of France',
-    'Cows are awesome animals',
-    'Trump was the best president in the history of US',
-    'Trump was the worst president in the history of US',
-    'Obama was the best president in the history of US'
+  List<Question> questionBank = [
+    Question('Paris is the capital of France', true),
+    Question('Cows are awesome animals', true),
+    Question('Trump was the best president in the history of US', false),
+    Question('Some cats are actually allergic to humans', true),
+    Question('You can lead a cow down stairs but not up stairs.', false),
+    Question('Approximately one quarter of human bones are in the feet.', true),
+    Question('A slug\'s blood is green.', true),
+    Question('Buzz Aldrin\'s mother\'s maiden name was \"Moon\".', true),
+    Question('It is illegal to pee in the Ocean in Portugal.', true),
+    Question(
+        'No piece of square dry paper can be folded in half more than 7 times.',
+        false),
+    Question(
+        'In London, UK, if you happen to die in the House of Parliament, you are technically entitled to a state funeral, because the building is considered too sacred a place.',
+        true),
+    Question(
+        'The loudest sound produced by any animal is 188 decibels. That animal is the African Elephant.',
+        false),
+    Question(
+        'The total surface area of two human lungs is approximately 70 square metres.',
+        true),
+    Question('Google was originally called \"Backrub\".', true),
+    Question(
+        'Chocolate affects a dog\'s heart and nervous system; a few ounces are enough to kill a small dog.',
+        true),
+    Question(
+        'In West Virginia, USA, if you accidentally hit an animal with your car, you are free to take it home to eat.',
+        true),
   ];
-  List<bool> answers = [true, true, false, false, false];
-  List<Widget> scoreKeeper = [];
+  List<Icon> scoreKeeper = [];
   int questionNumber = 0;
   @override
   Widget build(BuildContext context) {
@@ -47,7 +70,7 @@ class _QuizPageState extends State<QuizPage> {
             padding: EdgeInsets.all(10.0),
             child: Center(
               child: Text(
-                questions[questionNumber],
+                questionBank[questionNumber].questionText,
                 textAlign: TextAlign.center,
                 style: TextStyle(
                   fontSize: 25.0,
@@ -72,7 +95,7 @@ class _QuizPageState extends State<QuizPage> {
               ),
               onPressed: () {
                 setState(() {
-                  if (answers[questionNumber] == true) {
+                  if (questionBank[questionNumber].questionAnswer == true) {
                     scoreKeeper.add(
                       Icon(
                         Icons.check,
@@ -108,7 +131,7 @@ class _QuizPageState extends State<QuizPage> {
               ),
               onPressed: () {
                 setState(() {
-                  if (answers[questionNumber] == false) {
+                  if (questionBank[questionNumber].questionAnswer == false) {
                     scoreKeeper.add(
                       Icon(
                         Icons.close,
