7ded2c0b1e4f07ae096c7c5d6434bb6419cba4fc
Sodgerel Mandakhnaran <sm8694@nyu.edu>
2021-02-13 15:22:56 +0800

    Added quizbrain class

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
-import 'question.dart';
+import 'package:quizzler/quiz_brain.dart';
+import 'quiz_brain.dart';
 
 void main() => runApp(Quizzler());
 
@@ -26,38 +27,9 @@ class QuizPage extends StatefulWidget {
 }
 
 class _QuizPageState extends State<QuizPage> {
-  List<Question> questionBank = [
-    Question('Paris is the capital of France', true),
-    Question('Cows are awesome animals', true),
-    Question('Trump was the best president in the history of US', false),
-    Question('Some cats are actually allergic to humans', true),
-    Question('You can lead a cow down stairs but not up stairs.', false),
-    Question('Approximately one quarter of human bones are in the feet.', true),
-    Question('A slug\'s blood is green.', true),
-    Question('Buzz Aldrin\'s mother\'s maiden name was \"Moon\".', true),
-    Question('It is illegal to pee in the Ocean in Portugal.', true),
-    Question(
-        'No piece of square dry paper can be folded in half more than 7 times.',
-        false),
-    Question(
-        'In London, UK, if you happen to die in the House of Parliament, you are technically entitled to a state funeral, because the building is considered too sacred a place.',
-        true),
-    Question(
-        'The loudest sound produced by any animal is 188 decibels. That animal is the African Elephant.',
-        false),
-    Question(
-        'The total surface area of two human lungs is approximately 70 square metres.',
-        true),
-    Question('Google was originally called \"Backrub\".', true),
-    Question(
-        'Chocolate affects a dog\'s heart and nervous system; a few ounces are enough to kill a small dog.',
-        true),
-    Question(
-        'In West Virginia, USA, if you accidentally hit an animal with your car, you are free to take it home to eat.',
-        true),
-  ];
   List<Icon> scoreKeeper = [];
   int questionNumber = 0;
+  QuizBrain quizBrain = QuizBrain();
   @override
   Widget build(BuildContext context) {
     return Column(
@@ -70,7 +42,7 @@ class _QuizPageState extends State<QuizPage> {
             padding: EdgeInsets.all(10.0),
             child: Center(
               child: Text(
-                questionBank[questionNumber].questionText,
+                quizBrain.questionBank[questionNumber].questionText,
                 textAlign: TextAlign.center,
                 style: TextStyle(
                   fontSize: 25.0,
@@ -95,7 +67,8 @@ class _QuizPageState extends State<QuizPage> {
               ),
               onPressed: () {
                 setState(() {
-                  if (questionBank[questionNumber].questionAnswer == true) {
+                  if (quizBrain.questionBank[questionNumber].questionAnswer ==
+                      true) {
                     scoreKeeper.add(
                       Icon(
                         Icons.check,
@@ -131,10 +104,11 @@ class _QuizPageState extends State<QuizPage> {
               ),
               onPressed: () {
                 setState(() {
-                  if (questionBank[questionNumber].questionAnswer == false) {
+                  if (quizBrain.questionBank[questionNumber].questionAnswer ==
+                      false) {
                     scoreKeeper.add(
                       Icon(
-                        Icons.close,
+                        Icons.check,
                         color: Colors.green,
                       ),
                     );
